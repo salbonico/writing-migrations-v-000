196b0f1650bcbd19779567cdecf998431c98e0f6
Steve Albonico <salbonico@gmail.com>
2019-01-31 00:30:17 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class change_datatype_for_birthdate < ActiveRecord::Migration[5.1]
+class ChangeDataTypeForBirthdate < ActiveRecord::Migration[5.1]
 
 def change
 change_column :students, :birthdate, :datetime
